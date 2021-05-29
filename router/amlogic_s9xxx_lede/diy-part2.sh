b82a60852893d9c77d90422fa58c85456995b9f3
rastyu <48859414+rastyu@users.noreply.github.com>
2021-05-29 22:13:32 +0800

    Update diy-part2.sh

--- router/amlogic_s9xxx_lede/diy-part2.sh
+++ router/amlogic_s9xxx_lede/diy-part2.sh
@@ -8,7 +8,7 @@
 #========================================================================================================================
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.6/g' package/base-files/files/bin/config_generate
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 # sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
@@ -71,7 +71,7 @@ sed -i 's/\[\[fallthrough\]\]\;/\/\* fallthrough \*\//g' feeds/packages/net/ngin
 # git apply ../router-config/patches/{0001*,0002*}.patch --directory=feeds/luci
 
 # Modify some code adaptation
-# sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
 
 # Add luci-theme
 # svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod package/luci-theme-bootstrap-mod
