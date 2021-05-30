883aae46791e31cf61f964c52b0222de5f42d005
rastyu <48859414+rastyu@users.noreply.github.com>
2021-05-30 21:50:44 +0800

    Update diy-part2.sh

--- router/amlogic_s9xxx_lede/diy-part2.sh
+++ router/amlogic_s9xxx_lede/diy-part2.sh
@@ -7,8 +7,7 @@
 # Copyright (C) 2020 https://github.com/ophub/amlogic-s9xxx-openwrt
 #========================================================================================================================
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-svn co https://github.com/immortalwrt/luci/applications/luci-app-filebrowser package/luci-app-filebrowser
-svn co https://github.com/immortalwrt/packages/utils/filebrowser package/filebrowser
+git clone https://github.com/281677160/openwrt-package.git package/openwrt-packages
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
