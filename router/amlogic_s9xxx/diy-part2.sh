7d7c3349ffcc569588ef60d48b5ee4754e125a3d
rastyu <48859414+rastyu@users.noreply.github.com>
2021-08-05 23:41:27 +0800

    Update diy-part2.sh

--- router/amlogic_s9xxx/diy-part2.sh
+++ router/amlogic_s9xxx/diy-part2.sh
@@ -11,7 +11,6 @@ git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-
 #添加luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 #添加packages
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
 git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
@@ -22,14 +21,13 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser pack
 sed -i 's/192.168.1.1/192.168.10.6/g' package/base-files/files/bin/config_generate
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' ./feeds/luci/collections/luci/Makefile
 
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 # sed -i 's/root::0:0:99999:7:::/root:$1$R8PYVHeZ$SRRcQVgo.FSLaSE/mk.vD/:18790:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /ALLEN build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-#sed -i "s/OpenWrt /ALLEN build $(TZ=UTC-8 date "+%Y.%m.%d") /g" package/lean/default-settings/files/zzz-default-settings
 
 # Add luci-app-bypass
 # git clone https://github.com/garypang13/openwrt-bypass.git package/luci-app-bypass
