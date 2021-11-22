ac523f0a3c764c0d228b547d1f5f040e2a1ac2b1
rastyu <48859414+rastyu@users.noreply.github.com>
2021-11-22 14:11:39 +0800

    Update diy-part2.sh

--- router/HK1/diy-part2.sh
+++ router/HK1/diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.10.6）
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.10.6）
 sed -i 's/192.168.1.1/192.168.10.6/g' package/base-files/files/bin/config_generate
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
