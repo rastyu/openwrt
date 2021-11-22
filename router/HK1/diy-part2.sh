5a6460bf9100376d3e198cde4e48f8434c250d0b
rastyu <48859414+rastyu@users.noreply.github.com>
2021-11-22 14:18:42 +0800

    Update diy-part2.sh

--- router/HK1/diy-part2.sh
+++ router/HK1/diy-part2.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.10.6）
-sed -i 's/192.168.1.1/192.168.10.6/g' package/base-files/files/bin/config_generate
+cat >$NETIP <<-EOF
+uci set network.lan.ipaddr='192.168.10.6'                   # IPv4 地址(openwrt后台地址)
+uci set network.lan.netmask='255.255.255.0'                 # IPv4 子网掩码
+uci set network.lan.gateway='192.168.10.1'                  # IPv4 网关+
+uci commit network
+uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
+uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' ./feeds/luci/collections/luci/Makefile
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /ALLEN build@ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
