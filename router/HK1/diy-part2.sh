27497fd3891ce8b5a77a7e7d9c98c59fafa31778
rastyu <48859414+rastyu@users.noreply.github.com>
2022-01-19 00:03:07 +0800

    Update diy-part2.sh

--- router/HK1/diy-part2.sh
+++ router/HK1/diy-part2.sh
@@ -19,5 +19,10 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/lu
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /ALLEN build @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
+
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff  
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
+
+# Add autocore support for armvirt
+sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
