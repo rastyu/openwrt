0aae18cffa20d71fbe8b1143c74df80c8d8a7774
rastyu <48859414+rastyu@users.noreply.github.com>
2022-02-28 15:39:28 +0800

    Update diy-part2.sh

--- router/HK1/diy-part2.sh
+++ router/HK1/diy-part2.sh
@@ -13,8 +13,8 @@ sed -i 's/192.168.1.1/192.168.10.6/g' package/base-files/files/bin/config_genera
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
 sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' ./feeds/luci/collections/luci/Makefile
 
-# Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+# 空密码
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i "s/OpenWrt /ALLEN build @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
