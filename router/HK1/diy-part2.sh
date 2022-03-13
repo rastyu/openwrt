d1bbc55b28401c22c7e9d5a01513f93f2729571d
rastyu <48859414+rastyu@users.noreply.github.com>
2022-03-13 15:39:13 +0800

    Update diy-part2.sh

--- router/HK1/diy-part2.sh
+++ router/HK1/diy-part2.sh
@@ -17,10 +17,11 @@ sed -i 's/luci-theme-bootstrap/luci-theme-neobird/g' ./feeds/luci/collections/lu
 sed -i "s/OpenWrt /ALLEN build @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
 
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
+
+
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-git clone https://github.com/sun-cut/luci-app-nginx-manager.git package/luci-app-nginx-manager
+
+git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
 
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
