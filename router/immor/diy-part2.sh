dc64297e9353663f4bee98d0087032510883d707
rastyu <48859414+rastyu@users.noreply.github.com>
2022-04-23 20:04:02 +0800

    Add files via upload

---
+++ router/immor/diy-part2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+#========================================================================================================================
+
+ 
+ # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.10.6）
+sed -i 's/192.168.1.1/192.168.10.2/g' package/base-files/files/bin/config_generate
+
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
+# sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
+
+# 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
+sed -i "s/OpenWrt /ALLEN build @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
+
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+# git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# git clone https://github.com/fw876/helloworld package/helloworld
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+
+# Add themes
+# git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+# sed -i 's/shadowsocksr/passwall/g' package/luci-theme-neobird/luasrc/view/themes/neobird/header.htm 
