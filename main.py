a844a0ed533d92cf18663a1980676dd28f40483d
rastyu <48859414+rastyu@users.noreply.github.com>
2022-09-04 00:05:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from embyserver import embyserver
 
 #EMBY 域名 包含http(s)和端口号后面不带/
 #例如https://xxx.xxx.xxx:8920
-embyhost = ''
+embyhost = 'http://192.168.10.5:8096'
 #EMBY 用户ID 进EMBY 用户点击管理员账号配置可以在网址上看到userId
-embyuserid = ''
+embyuserid = '053e047de092441a9e0a8fe1951345a6'
 #EMBY APIKEY
-embykey = ''
+embykey = '799a6b5d48eb48cca88aae17917bd5c1'
 #TMDB APIKEY
-tmdbkey = ''
+tmdbkey = '3dc672cf452b813d8c97b26917b8b375'
 #线程数量
-threadnum = 16
+threadnum = 4
 #是否刷新人名
 updatepeople = True
 #每次刷新全部媒体间隔时间 [小时]
