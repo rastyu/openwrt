c90637bb488c4ac61801465e336942065c4cfc92
rastyu <48859414+rastyu@users.noreply.github.com>
2022-12-26 20:09:28 +0800

    Update immor-part2.sh

--- immor-part2.sh
+++ immor-part2.sh
@@ -14,7 +14,7 @@ sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_gener
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonne
 
-git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 # git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice package/luci-app-poweroffdevice
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
