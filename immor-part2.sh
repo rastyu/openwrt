3e931c25ad214282e45e24f78f57b3abc5b7fe8d
rastyu <48859414+rastyu@users.noreply.github.com>
2022-12-26 09:22:01 +0800

    Update immor-part2.sh

--- immor-part2.sh
+++ immor-part2.sh
@@ -11,8 +11,8 @@
 sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-# git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonne
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argonne
 
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 #git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice package/luci-app-poweroffdevice
