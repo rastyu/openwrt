5d6bf0d182935b6243d77cefda3a62f75155b6ca
rastyu <48859414+rastyu@users.noreply.github.com>
2022-12-27 11:45:14 +0800

    Update lede-part2.sh

--- lede-part2.sh
+++ lede-part2.sh
@@ -7,10 +7,10 @@
 #========================================================================================================================
 
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.10.6）
+# 默认IP地址（FROM 192.168.1.1 CHANGE TO 192.168.10.6）
 sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
 
-# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
+# 默认主题（FROM uci-theme-bootstrap CHANGE TO luci-theme-atmaterial）
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
 
 # 版本号里显示一个自己的名字（281677160 build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
