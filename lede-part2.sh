0ccfee6505f6cc8e97c802ac938a9b60f2bd8c7c
rastyu <48859414+rastyu@users.noreply.github.com>
2022-12-27 12:50:10 +0800

    Update lede-part2.sh

--- lede-part2.sh
+++ lede-part2.sh
@@ -28,8 +28,8 @@ svn co https://github.com/messense/aliyundrive-fuse/trunk/openwrt package/aliyun
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne package/luci-theme-argonne
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argonne-config package/luci-app-argonne-config
 
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
 
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-store package/luci-app-store
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-lib-taskd package/luci-lib-taskd
