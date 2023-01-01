3205556a9f7e8b00b723a30e06dd12bcd6739adb
rastyu <48859414+rastyu@users.noreply.github.com>
2023-01-01 15:07:24 +0800

    Update lede-part1.sh

--- lede-part1.sh
+++ lede-part1.sh
@@ -16,4 +16,4 @@
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # other
-# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+rm -rf package/lean/{luci-app-netdata}
