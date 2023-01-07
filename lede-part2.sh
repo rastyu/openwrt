3a1458a8a5fe396b408953a7482c21bc7a8c97c4
rastyu <48859414+rastyu@users.noreply.github.com>
2023-01-07 20:46:38 +0800

    Update lede-part2.sh

--- lede-part2.sh
+++ lede-part2.sh
@@ -19,42 +19,35 @@ sed -i "s/OpenWrt /ALLEN Build @/g" package/lean/default-settings/files/zzz-defa
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
 
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall package/luci-app-passwall
 
-git clone https://github.com/rastyu/package.git package/rastyu
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
-
+# 主题
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne package/luci-theme-argonne
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argonne-config package/luci-app-argonne-config
 
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-
+# 商店
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-store package/luci-app-store
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-lib-taskd package/luci-lib-taskd
 svn co https://github.com/kenzok8/openwrt-packages/trunk/taskd package/taskd
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-lib-xterm package/luci-lib-xterm
 
-
+git clone https://github.com/rastyu/package.git package/rastyu
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 
+# poweroff关机功能集成到系统源码菜单中
+curl -fsSL  https://raw.githubusercontent.com/rastyu/package/main/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
+curl -fsSL  https://raw.githubusercontent.com/rastyu/package/main/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+# 系统监控
 # rm -rf ./feeds/luci/applications/luci-app-netdata
 # git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
-
-# git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 
 # svn co https://github.com/kenzok8/small-package/trunk/luci-app-nginx-manager package/luci-app-nginx-manager
 # svn co https://github.com/kenzok8/openwrt-packages/trunk/filebrowser package/filebrowser
 # svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-filebrowser package/luci-app-filebrowser
+# 科学软件
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 # git clone https://github.com/fw876/helloworld package/helloworld
-
-# Add themes
-# git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-# sed -i 's/shadowsocksr/passwall/g' package/luci-theme-neobird/luasrc/view/themes/neobird/header.htm
-
-#在编译前,运行如下二条命令，集成到系统源码菜单中，不需要另外选择和设置即可使用关机功能。
-curl -fsSL  https://raw.githubusercontent.com/rastyu/package/main/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
-curl -fsSL  https://raw.githubusercontent.com/rastyu/package/main/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
