1818f136a2fda3ccdff25f6a907dd8cf57595b31
rastyu <48859414+rastyu@users.noreply.github.com>
2023-01-09 10:13:39 +0800

    Create official-part2.sh

---
+++ official-part2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+#========================================================================================================================
+
+# rm -rf ./feeds/luci/applications/luci-app-netdata
+# git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+
+# svn co https://github.com/kenzok8/small-package/trunk/luci-app-nginx-manager package/luci-app-nginx-manager
+# svn co https://github.com/kenzok8/openwrt-packages/trunk/filebrowser package/filebrowser
+# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-filebrowser package/luci-app-filebrowser
+# 科学软件
+# git clone https://github.com/jerrykuku/lua-maxminddb.git package/maxminddb
+# git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+# git clone https://github.com/fw876/helloworld package/helloworld
