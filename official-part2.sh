555485c28ce037856419087a7210593aadd4bc44
rastyu <48859414+rastyu@users.noreply.github.com>
2023-01-09 10:37:22 +0800

    Update official-part2.sh

--- official-part2.sh
+++ official-part2.sh
@@ -7,10 +7,7 @@
 #========================================================================================================================
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-# rm -rf ./feeds/luci/applications/luci-app-netdata
-# git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 
-# svn co https://github.com/kenzok8/small-package/trunk/luci-app-nginx-manager package/luci-app-nginx-manager
 # svn co https://github.com/kenzok8/openwrt-packages/trunk/filebrowser package/filebrowser
 # svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-filebrowser package/luci-app-filebrowser
 # 科学软件
