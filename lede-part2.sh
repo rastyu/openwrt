4b4b11204a1bdf12daedbf0861e06216217064a6
rastyu <48859414+rastyu@users.noreply.github.com>
2024-02-27 23:42:31 +0800

    Update lede-part2.sh

--- lede-part2.sh
+++ lede-part2.sh
@@ -61,9 +61,9 @@ merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-
 merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/taskd
 merge_package https://github.com/kenzok8/openwrt-packages openwrt-packages/luci-lib-xterm
 
-merge_package https://github.com/rastyu/package package
-merge_package https://github.com/sirpdboy/luci-app-ddns-go luci-app-ddns-go
-merge_package https://github.com/rufengsuixing/luci-app-adguardhome luci-app-adguardhome
+#merge_package https://github.com/rastyu/package package
+#merge_package https://github.com/sirpdboy/luci-app-ddns-go luci-app-ddns-go
+#merge_package https://github.com/rufengsuixing/luci-app-adguardhome luci-app-adguardhome
 #git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 
 # poweroff关机功能集成到系统源码菜单中
